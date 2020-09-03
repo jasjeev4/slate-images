62c4d7fdd14249c6c6811e56d5b7fdda9f1b346e
Jasjeev <jasjeev4@gmail.com>
2020-09-04 02:52:13 +0530

    Added files for the initial time

---
+++ home/static/home/_next/static/9ZIaFFJLonmmPyvmM3NRb/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fexamples\u002F[example]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ home/static/home/_next/static/9ZIaFFJLonmmPyvmM3NRb/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"23aj":function(n,e,u){"use strict";u.r(e);var t=u("mXGw"),c=u("bBV7");e.default=function(){var n=Object(c.useRouter)();return Object(t.useEffect)((function(){n.replace("/examples")})),null}},WjHl:function(n,e,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u("23aj")}])}},[["WjHl",0,2,1]]]);
